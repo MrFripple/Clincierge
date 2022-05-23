8980182c9d621b3eb7fbef5677c510ffd329d224
mathew dziedzic <mdzie195@gmail.com>
2022-05-22 21:34:47 -0700

    Implemented add image button

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,28 +1,29 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import GlobalContext from './context /Global';
 import FoodList from './FoodList';
 import axios from 'axios';
 
-
-
-
-
 function App() {
   const [foodImages, setFoodImages] = useState<string[]>([]);
 
 
-  useEffect( () => {
+  useEffect( () => 
+    addImage()
+  ,[]);
+
+  const addImage = () => {
     axios.get('/api')
     .then(({data}) => {
       setFoodImages([...foodImages, data.image])
     })
-    }
-  ,[]);
+  }
 
   return (
     <GlobalContext.Provider value={{foodImages, setFoodImages}}>
+      <h1>Food Images</h1>
       <FoodList />
+      <button onClick = {addImage}> Add Image</button>
     </GlobalContext.Provider>
   );
 }
